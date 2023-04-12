0fffc0a863eb49f8a29792974a5a893dfb5a79da
Serhii Denshchyk <serhii.denshchyk@gmail.com>
2023-04-12 14:56:36 +0300

    Add solution to task6

--- MultiThreading.Task6.Continuation/Program.cs
+++ MultiThreading.Task6.Continuation/Program.cs
@@ -7,6 +7,8 @@
    Demonstrate the work of the each case with console utility.
 */
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task6.Continuation
 {
@@ -22,9 +24,71 @@ namespace MultiThreading.Task6.Continuation
             Console.WriteLine("Demonstrate the work of the each case with console utility.");
             Console.WriteLine();
 
-            // feel free to add your code
+            CriteriaA();
+            CriteriaB();
+            CriteriaC();
+            CriteriaD();
 
             Console.ReadLine();
+        }
+
+        private static void CriteriaA()
+        {
+            var parentTask = Task.Factory.StartNew(() =>
+            {
+                return 2 + 2;
+            });
+
+            var continuationTask = parentTask.ContinueWith(parent =>
+            {
+                Console.WriteLine($"{nameof(CriteriaA)}: Continuation task should be executed regardless of the result of the parent task.");
+                Console.WriteLine($"{nameof(CriteriaA)}: Parent task result: {parent.Result}");
+            });
+        }
+
+        private static void CriteriaB()
+        {
+            var parentTask = Task.Factory.StartNew(() =>
+            {
+                throw new Exception("Something bad happened.");
+            });
+
+            var continuationTask = parentTask.ContinueWith(parent =>
+            {
+                Console.WriteLine($"{nameof(CriteriaB)}: Continuation task should be executed when the parent task finished without success.");
+                Console.WriteLine($"{nameof(CriteriaB)}: Parent task exception: {parent.Exception.InnerException.Message}");
+            }, TaskContinuationOptions.OnlyOnFaulted);
+        }
+        
+        private static void CriteriaC()
+        {
+            var parentTask = Task.Factory.StartNew(() =>
+            {
+                throw new Exception("Something bad happened.");
+            });
+
+            var continuationTask = parentTask.ContinueWith(parent =>
+            {
+                Console.WriteLine($"{nameof(CriteriaC)}: Continuation task should be executed when the parent task would be finished with fail and parent task thread should be reused for continuation");
+                Console.WriteLine($"{nameof(CriteriaC)}: Parent task exception: {parent.Exception.InnerException.Message}");
+            }, TaskContinuationOptions.OnlyOnFaulted | TaskContinuationOptions.ExecuteSynchronously);
+        }
+        
+        private static void CriteriaD()
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+            
+            var parentTask = Task.Factory.StartNew(() =>
+            {
+                cts.Cancel();
+                cts.Token.ThrowIfCancellationRequested();
+            }, token);
+
+            var continuationTask = parentTask.ContinueWith(parent =>
+            {
+                Console.WriteLine($"{nameof(CriteriaD)}: Continuation task should be executed outside of the thread pool when the parent task would be cancelled");
+            }, TaskContinuationOptions.OnlyOnCanceled | TaskContinuationOptions.LongRunning);
         }
     }
 }
