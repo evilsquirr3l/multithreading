dbf1572e1053c793203f323ebf6277d93fd29115
Serhii Denshchyk <serhii.denshchyk@gmail.com>
2023-04-12 13:34:24 +0300

    Add solution to task5

--- MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -5,11 +5,16 @@
  * Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.
  */
 using System;
+using System.Collections.Concurrent;
+using System.Threading;
 
 namespace MultiThreading.Task5.Threads.SharedCollection
 {
     class Program
     {
+        private static readonly ConcurrentBag<int> SharedCollection = new();
+        private static readonly object SyncLock = new();
+        
         static void Main(string[] args)
         {
             Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
@@ -17,9 +22,52 @@ namespace MultiThreading.Task5.Threads.SharedCollection
             Console.WriteLine("Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.");
             Console.WriteLine();
 
-            // feel free to add your code
+            var addThread = new Thread(AddItems);
+            var printThread = new Thread(PrintItems);
+
+            addThread.Start();
+            printThread.Start();
+
+            addThread.Join();
+            printThread.Join();
 
             Console.ReadLine();
+        }
+
+        private static void AddItems()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                lock (SyncLock)
+                {
+                    SharedCollection.Add(i);
+                    Console.WriteLine($"Item {i} added.");
+                    Monitor.Pulse(SyncLock);
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+        private static void PrintItems()
+        {
+            var itemCount = 0;
+
+            while (itemCount < 10)
+            {
+                lock (SyncLock)
+                {
+                    while (SharedCollection.IsEmpty)
+                    {
+                        Monitor.Wait(SyncLock);
+                    }
+
+                    if (SharedCollection.TryTake(out var item))
+                    {
+                        Console.WriteLine($"Item {item} printed.");
+                        itemCount++;
+                    }
+                }
+            }
         }
     }
 }
